f100b1ab5eade52ad29ce3773cbf569607f4f8de
cpaleop <cpaleop@arx.net>
2020-09-15 20:40:37 +0300

    Introduce flag/lock and retry mechanism to avoid conflicts with concurrent refresh token. Keep logs for further testing.

--- core/src/main/java/gr/cpaleop/core/data/interceptors/RefreshTokenInterceptor.kt
+++ core/src/main/java/gr/cpaleop/core/data/interceptors/RefreshTokenInterceptor.kt
@@ -5,44 +5,81 @@ import gr.cpaleop.core.domain.repositories.AuthenticationRepository
 import gr.cpaleop.core.domain.repositories.PreferencesRepository
 import gr.cpaleop.core.domain.repositories.PreferencesRepository.Companion.ACCESS_TOKEN
 import gr.cpaleop.core.domain.repositories.PreferencesRepository.Companion.REFRESH_TOKEN
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
+import timber.log.Timber
 
+@Suppress("BlockingMethodInNonBlockingContext")
 class RefreshTokenInterceptor(
     private val preferencesRepository: PreferencesRepository,
     private val authenticationRepository: AuthenticationRepository
 ) : Interceptor {
+
+    private var isRefreshing: Boolean = false
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
 
         val originalResponse = chain.proceed(originalRequest)
 
-        if (isTokenExpired(originalResponse)) {
-            return runBlocking(Dispatchers.IO) {
-                val newToken =
-                    authenticationRepository.refreshToken(
-                        preferencesRepository.getString(
-                            REFRESH_TOKEN
-                        )
-                    )
-                saveToken(newToken)
-                return@runBlocking chain.proceed(originalRequest)
+        return runBlocking {
+            if (isTokenExpired(originalResponse)) {
+                Timber.e("SKATA Token expired")
+                if (!isRefreshing) {
+                    Timber.e("SKATA Token expired and not refreshing. Time to refresh")
+                    refreshToken()
+                    return@runBlocking chain.proceed(originalRequest)
+                } else {
+                    return@runBlocking repeatOriginalRequest(chain, originalRequest)
+                }
+            } else {
+                return@runBlocking originalResponse
             }
-        } else {
-            return originalResponse
         }
     }
 
-    private fun isTokenExpired(response: Response): Boolean {
-        return response.code == 401 || response.code == 4001
+    private fun isTokenExpired(response: Response): Boolean =
+        response.code == UNAUTHORIZED_HTTP_ERROR
+
+    private suspend fun refreshToken() {
+        Timber.e("SKATA Refreshing token")
+        isRefreshing = true
+        val newToken =
+            authenticationRepository.refreshToken(
+                preferencesRepository.getString(
+                    REFRESH_TOKEN
+                )
+            )
+        saveToken(newToken)
+        isRefreshing = false
     }
 
-    @Synchronized
+    private suspend fun repeatOriginalRequest(
+        chain: Interceptor.Chain,
+        originalRequest: Request
+    ): Response {
+        var retryCount = 1
+        var response: Response
+        do {
+            Timber.e("SKATA Retrying original request")
+            delay(RETRY_DELAY_MS)
+            response = chain.proceed(originalRequest)
+        } while (++retryCount < RETRY_MAX_TRIES && isTokenExpired(response))
+        return response
+    }
+
     private suspend fun saveToken(newToken: Token) {
         preferencesRepository.putString(ACCESS_TOKEN, newToken.accessToken)
         preferencesRepository.putString(REFRESH_TOKEN, newToken.refreshToken)
+    }
+
+    companion object {
+
+        private const val UNAUTHORIZED_HTTP_ERROR = 401
+        private const val RETRY_DELAY_MS: Long = 3000
+        private const val RETRY_MAX_TRIES = 3
     }
 }
