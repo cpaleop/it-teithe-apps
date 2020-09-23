549ceba61533cb46ede694367bb68010a923c2f6
cpaleop <cpaleop@arx.net>
2020-09-23 05:14:06 +0300

    Revert because usecase breaks Flow with invalidation of paging data source

--- dashboard/src/main/java/gr/cpaleop/dashboard/di/DashboardModule.kt
+++ dashboard/src/main/java/gr/cpaleop/dashboard/di/DashboardModule.kt
@@ -83,12 +83,13 @@ val dashboardModule = module {
     single<GetSavedDocumentsUseCase> { GetSavedDocumentsUseCaseImpl(get(), get()) }
     single<GetProfileUseCase> { GetProfileUseCaseImpl(get()) }
     single<GetNotificationsUseCase> { GetNotificationsUseCaseImpl(get(), get()) }
-    single<ObserveAnnouncementsUseCase> { ObserveAnnouncementsUseCaseImpl(get(), get()) }
+    single<ObserveAnnouncementsUseCase> { ObserveAnnouncementsUseCaseImpl(get()) }
     single<CategoriesRepository> { CategoriesRepositoryImpl(get(), get(), get(), get()) }
     single<DeviceStorageRepository> { DeviceStorageRepositoryImpl(get()) }
     single<NotificationsRepository> { NotificationsRepositoryImpl(get(), get()) }
     single<AnnouncementsRepository> {
         AnnouncementsRepositoryImpl(
+            get(),
             get(),
             get(),
             get(),
--- dashboard/src/main/java/gr/cpaleop/dashboard/domain/usecases/ObserveAnnouncementsUseCaseImpl.kt
+++ dashboard/src/main/java/gr/cpaleop/dashboard/domain/usecases/ObserveAnnouncementsUseCaseImpl.kt
@@ -3,17 +3,13 @@ package gr.cpaleop.dashboard.domain.usecases
 import androidx.paging.PagingData
 import gr.cpaleop.core.domain.entities.Announcement
 import gr.cpaleop.dashboard.domain.repositories.AnnouncementsRepository
-import gr.cpaleop.dashboard.domain.repositories.PreferencesRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 
-class ObserveAnnouncementsUseCaseImpl(
-    private val announcementsRepository: AnnouncementsRepository,
-    private val preferencesRepository: PreferencesRepository
-) : ObserveAnnouncementsUseCase {
+class ObserveAnnouncementsUseCaseImpl(private val announcementsRepository: AnnouncementsRepository) :
+    ObserveAnnouncementsUseCase {
 
     override suspend fun invoke(coroutineScope: CoroutineScope): Flow<PagingData<Announcement>> {
-        announcementsRepository.sort(preferencesRepository.getAnnouncementSort())
         return announcementsRepository.getAnnouncements(coroutineScope)
     }
 }
